da7677190d14db38df3b557d280e37f6098d97b2
Harsh <sharshvardhan759@gmail.com>
2023-08-30 15:22:38 +0530

    just hard coded the buttons by assigning 1st entry of the data model to them.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quizzyy/answer_button.dart';
+import 'package:quizzyy/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,23 +14,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double
           .infinity, // assigning this column the whole screen we can also do "mainAxisSize: MainAxisSize.min" inside the column.
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("The Question"),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.black),
+          ),
           const SizedBox(height: 30),
-          
-          AnswerButton(answerText: "Answer1", onTap: (){}),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
           const SizedBox(height: 10),
-          AnswerButton(answerText: "Answer1", onTap: (){}),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
           const SizedBox(height: 10),
-          AnswerButton(answerText: "Answer1", onTap: (){}),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
           const SizedBox(height: 10),
-          AnswerButton(answerText: "Answer1", onTap: (){}),
-
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
         ],
       ),
     );
