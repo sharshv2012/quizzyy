258d36ee799ec213a500d5d5488ad6cb1207337b
Harsh <sharshvardhan759@gmail.com>
2023-09-02 19:45:56 +0530

    Commit 9 : Took answers as input from the buttons and added them in selectedAnswers list via a method passed from Quizz to QuestionScreen.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,10 +1,16 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quizzyy/answer_button.dart';
 import 'package:quizzyy/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quizzyy/models/quiz_questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectAnswer,
+  });
+
+  final void Function(String answer) onSelectAnswer;
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
@@ -13,12 +19,17 @@ class QuestionsScreen extends StatefulWidget {
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
-  void answerQuestion(){
-    setState(() { //when the answerQues method will run it'll set the state again.
-      currentQuestionIndex += 1;
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer); // use widget for using argument method in the stateful widget.
+    setState(() {
+      //when the answerQues method will run it'll set the state again.
+      if(currentQuestionIndex < questions.length - 1){
+        currentQuestionIndex += 1; 
+      }
+      
     });
-    
   }
+
   @override
   Widget build(context) {
     final currentQuestion = questions[currentQuestionIndex];
@@ -30,17 +41,27 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.black),
+              style: GoogleFonts.lato(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
-            ...currentQuestion.getShuffledAnswers().map((answer) { //... is used for spreading (takes all values from list or itrables and are added as , seperated individual items.)
-              return AnswerButton(answerText: answer, onTap: answerQuestion,);
-            }),// map will pass the provided functions or any passed functionality to it's listItems. 
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              //... is used for spreading (takes all values from list or itrables and are added as , seperated individual items.)
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+
+                  answerQuestion(answer);
+
+                },
+              );
+            }), // map will pass the provided functions or any passed functionality to it's listItems.
             /* this was hardcoded 
             AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
             const SizedBox(height: 10),
